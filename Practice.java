5f6ad040a261448f68906b2c095f8d759e303df8
Vishal Jumnani <vishalj64@users.noreply.github.com>
2023-02-19 09:59:29 +0530

    add-prac-file-to-start

---
+++ Practice.java
@@ -0,0 +1,14 @@
+class Practice {
+	
+	public static void main(String[] args) {
+
+		int age = 55;
+
+		System.out.println(age);
+
+		boolean isNomineeRegistered = false;
+
+System.out.println(isNomineeRegistered);
+
+	}
+}
